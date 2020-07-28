169b309ff39b13219bb94ff62ae04fe6ae33d8c9
Yuki Kokubo <yuukikokubo.pg@gmail.com>
2020-07-28 18:06:45 +0900

    READMEの実装終了

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import {AppRegistry, View, Text} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+const Share = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: 'yellowgreen'}}>
+      <Text>Hello Share Extension</Text>
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('MyShareEx', () => Share);
